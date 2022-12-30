2aa204b1b8c61773b2785abab31c710cb56640ca
Scott Canoni <scottcanoni@gmail.com>
2022-12-30 01:04:32 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Usage example with ExpressJS
+const express = require('express');
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+const host = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+
+const app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static('./'));
+
+const galleryDir = 'gallery';
+
+app.use(`/${galleryDir}`, require('./lib/gallery.js')({
+    staticFiles: `resources/${galleryDir}`,
+    urlRoot: galleryDir,
+    title: 'TRON Collection',
+    render: false, //
+}), function (req, res, next) {
+    console.log(`Returning response for ${req.originalUrl}`);
+    return res.render('gallery', { galleryHtml: req.html });
+});
+
+app.listen(port, host);
+
+console.log(`Web server listening on http://${host}:${port}/${galleryDir}`);
